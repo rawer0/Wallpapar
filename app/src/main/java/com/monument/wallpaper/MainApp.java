276e7fbdd8192895980150220712544eeda814f5
rawer <rawer.dev@gmail.com>
2017-07-09 23:18:58 +0800

    remove unused code

--- app/src/main/java/com/monument/wallpaper/MainApp.java
+++ app/src/main/java/com/monument/wallpaper/MainApp.java
@@ -2,6 +2,8 @@ package com.monument.wallpaper;
 
 import android.app.Application;
 
+import com.ovwvwvo.jkit.log.LogUtil;
+
 /**
  * Copyright ©2017 by rawer
  */
@@ -11,5 +13,7 @@ public class MainApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        LogUtil.setEnable(true);
     }
 }
