d5a4046c4d496dbafdaad9ea7308a59ea4dbe613
rawer <rawer.dev@gmail.com>
2017-07-09 23:53:15 +0800

    add bmob

--- app/src/main/java/com/monument/wallpaper/presenter/LoadDataPresenterImpl.java
+++ app/src/main/java/com/monument/wallpaper/presenter/LoadDataPresenterImpl.java
@@ -4,6 +4,10 @@ import com.monument.wallpaper.model.UrlModel;
 
 import java.util.List;
 
+import cn.bmob.v3.BmobQuery;
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.QueryListener;
+
 /**
  * Copyright ©2017 by rawer
  */
@@ -12,6 +16,16 @@ public class LoadDataPresenterImpl implements LoadDataPresenter {
 
     @Override
     public void loadData() {
+        BmobQuery<UrlModel> bmobQuery = new BmobQuery<UrlModel>();
+        bmobQuery.getObject("6b6c11c537", new QueryListener<UrlModel>() {
+            @Override
+            public void done(UrlModel object, BmobException e) {
+                if (e == null) {
+
+                } else {
+                }
+            }
+        });
     }
 
 
