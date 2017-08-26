4e645ff6620197308ed2be4ade9f3e9ea87f5528
rawer <rawer.dev@gmail.com>
2017-08-26 16:06:29 +0800

    ADD: init Bmob

--- app/src/main/java/com/ovwvwvo/wallpaper/MainApp.java
+++ app/src/main/java/com/ovwvwvo/wallpaper/MainApp.java
@@ -1,9 +1,12 @@
 package com.ovwvwvo.wallpaper;
 
 import android.app.Application;
+import android.content.Context;
 
-import com.google.firebase.FirebaseApp;
+import com.ovwvwvo.jkit.AppWrapper;
 import com.ovwvwvo.jkit.log.LogUtil;
+
+import java.lang.reflect.Method;
 
 /**
  * Copyright ©2017 by rawer
@@ -15,9 +18,17 @@ public class MainApp extends Application {
     public void onCreate() {
         super.onCreate();
 
+        AppWrapper.getInstance().setAppContext(this);
         LogUtil.setEnable(true);
 
-        if ("firebase".equals(BuildConfig.FLAVOR))
-            FirebaseApp.initializeApp(this, null);
+        if ("bmob".equals(BuildConfig.FLAVOR)) {
+            try {
+                Class clazz = Class.forName("cn.bmob.v3.Bmob");
+                Method method = clazz.getMethod("initialize", Context.class, String.class);
+                method.invoke(null, this, "0e9e903b300da967bc0032cec8359895");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
