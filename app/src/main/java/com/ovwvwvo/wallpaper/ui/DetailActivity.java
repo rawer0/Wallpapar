05efd8b38dc64ce94de7a4fa53c0fdf678d07d77
rawer <rawer.dev@gmail.com>
2017-10-22 00:54:34 +0800

    DEL - delete unused code

--- app/src/main/java/com/ovwvwvo/wallpaper/ui/DetailActivity.java
+++ app/src/main/java/com/ovwvwvo/wallpaper/ui/DetailActivity.java
@@ -3,8 +3,6 @@ package com.ovwvwvo.wallpaper.ui;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
-import android.view.Window;
-import android.view.WindowManager;
 
 import com.ovwvwvo.wallpaper.R;
 import com.ovwvwvo.wallpaper.adapter.DetailAdapter;
@@ -26,7 +24,6 @@ public class DetailActivity extends BaseActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.acitivity_detail);
         ButterKnife.bind(this);
@@ -36,24 +33,5 @@ public class DetailActivity extends BaseActivity {
         DetailAdapter detailAdapter = new DetailAdapter(getSupportFragmentManager(), urls);
         mViewPager.setAdapter(detailAdapter);
         mViewPager.setCurrentItem(position);
-
-
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-//            WindowManager.LayoutParams localLayoutParams = getWindow().getAttributes();
-//            ViewPager.LayoutParams.flags = (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | localLayoutParams.flags);
-//            if(Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP){
-//                //将侧边栏顶部延伸至status bar
-//                mDrawerLayout.setFitsSystemWindows(true);
-//                //将主页面顶部延伸至status bar;虽默认为false,但经测试,DrawerLayout需显示设置
-//                mDrawerLayout.setClipToPadding(false);
-//            }
-//        }
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
     }
 }
